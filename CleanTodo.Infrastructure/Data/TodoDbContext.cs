f2190086ce212f559ec90996942bc5b6c1ef03df
Jason L. Walker <jasonlanewalker@gmail.com>
2022-12-15 21:57:13 -0600

    Added db context and connection string for dev db

---
+++ CleanTodo.Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CleanTodo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Infrastructure.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+        public DbSet<TodoTag> TodoTags { get; set; }
+
+        public TodoDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
+            base.OnConfiguring(options);
+    }
+}
