3fd05a619ebc6516f7f020c6ac30dfb3622fa834
Jason L. Walker <jasonlanewalker@gmail.com>
2022-12-15 21:20:00 -0600

    Added config for todo item

---
+++ CleanTodo.Infrastructure/Data/Configuration/TodoItemConfig.cs
@@ -0,0 +1,21 @@
+﻿using CleanTodo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Infrastructure.Data.Configuration
+{
+    internal class TodoItemConfig : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.Property(x => x.Description).HasMaxLength(500);
+            builder.Property(x => x.DueDate).HasDefaultValue(DateTime.Now);
+            builder.HasMany(x => x.Tags);
+        }
+    }
+}
