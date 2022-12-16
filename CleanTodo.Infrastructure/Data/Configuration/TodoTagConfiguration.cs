815a1a9960ab9ea4e8736d314606319a84138f40
Jason L. Walker <jasonlanewalker@gmail.com>
2022-12-15 21:32:30 -0600

    Added todo tag config

---
+++ CleanTodo.Infrastructure/Data/Configuration/TodoTagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CleanTodo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Infrastructure.Data.Configuration
+{
+    internal class TodoTagConfiguration : IEntityTypeConfiguration<TodoTag>
+    {
+        public void Configure(EntityTypeBuilder<TodoTag> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(32);
+        }
+    }
+}
