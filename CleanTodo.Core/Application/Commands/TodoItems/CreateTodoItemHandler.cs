58b7777aa99d0bdab94a02e7a7e1140ca8562813
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-06 20:16:04 -0600

    Inject context interface into handler

--- CleanTodo.Core/Application/Commands/TodoItems/CreateTodoItemHandler.cs
+++ CleanTodo.Core/Application/Commands/TodoItems/CreateTodoItemHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using CleanTodo.Core.Application.Interfaces;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace CleanTodo.Core.Application.Commands.TodoItems
     public record CreateTodoItemCommand(CreateTodoItemRequest Data) : IRequest<TodoItemResponse>;
     public class CreateTodoItemHandler : IRequestHandler<CreateTodoItemCommand, TodoItemResponse>
     {
+        private readonly ITodoApplicationDbContext _dbContext;
+
+        public CreateTodoItemHandler(ITodoApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public Task<TodoItemResponse> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
         {
             return Task.FromResult(new TodoItemResponse { Id = Guid.NewGuid() });
