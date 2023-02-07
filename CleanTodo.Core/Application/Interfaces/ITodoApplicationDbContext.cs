6286671d19bd5da673a382e73d12e69393ba9ea3
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-06 19:47:59 -0600

    Created beginning of DB context interface

---
+++ CleanTodo.Core/Application/Interfaces/ITodoApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanTodo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Interfaces
+{
+    public interface ITodoApplicationDbContext
+    {
+        T Add<T>(T entity) where T : BaseEntity;
+    }
+}
