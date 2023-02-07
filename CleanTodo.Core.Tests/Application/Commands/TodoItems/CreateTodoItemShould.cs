c6240f34bb229a8aa482e7418a6cf0c958560682
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-06 20:28:52 -0600

    Mocked out handler

--- CleanTodo.Core.Tests/Application/Commands/TodoItems/CreateTodoItemShould.cs
+++ CleanTodo.Core.Tests/Application/Commands/TodoItems/CreateTodoItemShould.cs
@@ -1,5 +1,8 @@
 ﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using CleanTodo.Core.Application.Interfaces;
 using FluentAssertions;
+using MediatR;
+using Moq;
 using Xunit;
 
 namespace CleanTodo.Core.Tests.Application.Commands.TodoItems
@@ -15,9 +18,11 @@ namespace CleanTodo.Core.Tests.Application.Commands.TodoItems
             };
 
             var command = new CreateTodoItemCommand(request);
-            var handler = new CreateTodoItemHandler();
-            var response = handler.Handle(command, new CancellationToken()).Result;
-
+            var handler = new Mock<IRequestHandler<CreateTodoItemCommand, TodoItemResponse>>();
+            handler.Setup(x => x.Handle(command, new CancellationToken())).Returns(Task.FromResult(new TodoItemResponse() { Id = Guid.NewGuid() }));
+            
+            var response = handler.Object.Handle(command, new CancellationToken()).Result;
+            
             response.Id.Should().NotBe(default(Guid));
         }
     }
