d3e34298107d6824241b175f4d2edb3766b438ab
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-08 22:06:57 -0600

    Incorporated MediatR in the tests

--- CleanTodo.IntegrationTests/Application/Commands/TodoItems/CreateTodoItemShould.cs
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/CreateTodoItemShould.cs
@@ -2,9 +2,11 @@
 using CleanTodo.Core.Application.Commands.TodoItems;
 using CleanTodo.Infrastructure.Data;
 using FluentAssertions;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
@@ -12,32 +14,26 @@ namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
     public sealed class CreateTodoItemShould
     {
         private readonly TestWebApplicationFactory _factory;
-        private readonly TodoDbContext _db;
         private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMediator _mediator;
 
         public CreateTodoItemShould()
         {
             _factory = new TestWebApplicationFactory();
             _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
-            _db = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<TodoDbContext>();
+            _mediator = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<IMediator>();
         }
 
         [Fact]
-        public void GivenValidTodoItemRequest_WhenHandled_WillSucceed()
+        public async Task GivenValidTodoItemRequest_WhenHandled_WillSucceed()
         {
-            using var scope = _scopeFactory.CreateScope();
-            var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
-
             var request = new CreateTodoItemRequest()
             {
                 Description = "This is a todo item.",
             };
 
-            var command = new CreateTodoItemCommand(request);
-            var handler = new CreateTodoItemHandler(_db, mapper);
-            
-            var response = handler.Handle(command, new CancellationToken()).Result;
-            
+            var response = await _mediator.Send(new CreateTodoItemCommand(request));
+
             response.Id.Should().NotBe(default(Guid));
         }
     }
