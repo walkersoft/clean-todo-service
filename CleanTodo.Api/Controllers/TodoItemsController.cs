9f96a946fb0ab333c45f9b584696a790b413cb06
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-10 18:39:12 -0600

    Added controller endpoint to create a todo item

---
+++ CleanTodo.Api/Controllers/TodoItemsController.cs
@@ -0,0 +1,26 @@
+﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTodo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodoItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [ProducesResponseType(typeof(TodoItemResponse), StatusCodes.Status200OK)]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTodoItemRequest request)
+        {
+            return Ok(await _mediator.Send(new CreateTodoItemCommand(request)));
+        }
+    }
+}
