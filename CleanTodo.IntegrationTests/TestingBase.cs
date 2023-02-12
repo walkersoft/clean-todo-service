401cf476e191d3e40250d8740fc3dfbfcdfa6b30
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-12 11:42:37 -0600

    Fixed DB creation/deletion logic for tests

--- CleanTodo.IntegrationTests/TestingBase.cs
+++ CleanTodo.IntegrationTests/TestingBase.cs
@@ -1,5 +1,6 @@
 ﻿using CleanTodo.Infrastructure.Data;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -9,25 +10,33 @@ using System.Threading.Tasks;
 
 namespace CleanTodo.IntegrationTests
 {
-    abstract public class TestingBase
+    abstract public class TestingBase : IDisposable
     {
         private readonly TestWebApplicationFactory _factory;
         private readonly IServiceScope _serviceScope;
+        private TodoDbContext _dbContext;
         protected readonly IMediator _mediator;
+        
 
         public TestingBase()
         {
             _factory = new TestWebApplicationFactory();            
             _serviceScope = _factory.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();            
             _mediator = _serviceScope.ServiceProvider.GetRequiredService<IMediator>();
+            _dbContext = _serviceScope.ServiceProvider.GetRequiredService<TodoDbContext>();
             InitalizeDatabase();
         }
 
         private void InitalizeDatabase()
         {
-            var db = _serviceScope.ServiceProvider.GetRequiredService<TodoDbContext>();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
+            _dbContext.Database.Migrate();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Dispose();
+            GC.SuppressFinalize(this); // See CA1816
         }
     }
 }
