4fb34466f797126d523507857cc7905b81fa110c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-13 11:40:37 -0600

    Added todo tag request object

---
+++ CleanTodo.Core/Application/Commands/TodoTags/CreateTodoTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Commands.TodoTags
+{
+    public class CreateTodoTagRequest
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
