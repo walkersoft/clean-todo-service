d177bd8dc8a6fc9f97c374657243689bea53d794
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-13 12:54:56 -0600

    Implemented functionality to fetch all the todo tags

--- CleanTodo.Core/Application/Queries/TodoTags/GetAllTodoTagsQuery.cs
+++ CleanTodo.Core/Application/Queries/TodoTags/GetAllTodoTagsQuery.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using CleanTodo.Core.Application.Interfaces.Persitence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,22 @@ namespace CleanTodo.Core.Application.Queries.TodoTags
 
     public class GetAllTodoTagsQueryHandler : IRequestHandler<GetAllTodoTagsQuery, IEnumerable<TodoTagResponse>>
     {
-        public Task<IEnumerable<TodoTagResponse>> Handle(GetAllTodoTagsQuery request, CancellationToken cancellationToken)
+        private readonly ITodoApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetAllTodoTagsQueryHandler(ITodoApplicationDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<TodoTagResponse>> Handle(GetAllTodoTagsQuery request, CancellationToken cancellationToken)
+        {
+            var tags = await _context.TodoTags
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return tags.Select(tag => _mapper.Map<TodoTagResponse>(tag));
         }
     }
 }
