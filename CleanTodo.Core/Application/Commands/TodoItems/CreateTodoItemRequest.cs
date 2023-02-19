cc5e7208243f17d8bc4e4345e54421d3f5f73f00
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-19 15:18:18 -0600

    Fix name for date property

--- CleanTodo.Core/Application/Commands/TodoItems/CreateTodoItemRequest.cs
+++ CleanTodo.Core/Application/Commands/TodoItems/CreateTodoItemRequest.cs
@@ -8,7 +8,7 @@ namespace CleanTodo.Core.Application.Commands.TodoItems
         public string Description { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         public bool RollsOver { get; set; }
-        public DateTime DateDate { get; set; }
+        public DateTime DueDate { get; set; }
         public ICollection<Guid> TagIds { get; set; } = new List<Guid>();
     }
 }
