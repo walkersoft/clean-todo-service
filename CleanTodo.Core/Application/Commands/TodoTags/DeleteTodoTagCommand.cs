a40c2f8235a078d6a941bfe23b4ac17fe2639c85
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-21 23:15:37 -0600

    Added command to delete a todo tag

---
+++ CleanTodo.Core/Application/Commands/TodoTags/DeleteTodoTagCommand.cs
@@ -0,0 +1,32 @@
+﻿using CleanTodo.Core.Application.Interfaces.Persitence;
+using CleanTodo.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Commands.TodoTags
+{
+    public record DeleteTodoTagCommand(Guid Id) : IRequest<Unit>;
+
+    public class DeleteTodoTagCommandHandler : IRequestHandler<DeleteTodoTagCommand, Unit>
+    {
+        private readonly ITodoApplicationDbContext _context;
+
+        public DeleteTodoTagCommandHandler(ITodoApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteTodoTagCommand request, CancellationToken cancellationToken)
+        {
+            var tag = await _context.FirstOrNotFound<TodoTag>(request.Id);
+            _context.TodoTags.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
