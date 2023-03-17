2dfc15ceb311753ec25a655c824318d13f2e9f0c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-03-16 19:04:42 -0500

    Created start of todo item update test

---
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/UpdateTodoItemShould.cs
@@ -0,0 +1,24 @@
+﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
+{
+    public class UpdateTodoItemShould : TestingBase
+    {
+        public UpdateTodoItemShould() : base() { }
+
+        [Fact]
+        public async Task GivenExistingTodoItemIsEdited_WhenHandled_ItemIsUpdated()
+        {
+            var createRequest = new CreateTodoItemRequest() { Description = "Foo" };
+            var createResponse = await _mediator.Send(new CreateTodoItemCommand(createRequest));
+
+            _dbContext.ChangeTracker.Clear();
+        }
+    }
+}
