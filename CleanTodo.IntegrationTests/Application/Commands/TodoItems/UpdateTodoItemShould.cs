4781fe004a749866361eecbc2f4d43e9f7721cb2
Jason L. Walker <jasonlanewalker@gmail.com>
2023-03-17 21:59:19 -0500

    Wrote additional test to verify that the entirety of a todo item could be edited

--- CleanTodo.IntegrationTests/Application/Commands/TodoItems/UpdateTodoItemShould.cs
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/UpdateTodoItemShould.cs
@@ -1,4 +1,6 @@
 ﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using CleanTodo.Core.Application.Commands.TodoTags;
+using CleanTodo.Core.Application.Queries.TodoItems;
 using FluentAssertions;
 using System;
 using System.Collections.Generic;
@@ -30,6 +32,49 @@ namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
 
             updateResponse.Id.Should().Be(createResponse.Id);
             updateResponse.Description.Should().Be(updateRequest.Description);
+        }
+
+        [Fact]
+        public async Task GivenExistingTodoItemThatChangedCompletely_WhenHandled_ItemIsUpdated()
+        {
+            var createFooTagResponse = await _mediator.Send(new CreateTodoTagCommand(new TodoTagRequest() { Name = "Foo" }));
+            var createBarTagResponse = await _mediator.Send(new CreateTodoTagCommand(new TodoTagRequest() { Name = "Bar" }));
+
+            var createTodoItemRequest = new TodoItemRequest()
+            {
+                Description = "Foo",
+                IsActive = true,
+                DueDate = DateTime.Today,
+                RollsOver = true
+            };
+            createTodoItemRequest.TagIds.Add(createFooTagResponse.Id);
+
+            var createTodoItemResponse = await _mediator.Send(new CreateTodoItemCommand(createTodoItemRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var updateTodoItemRequest = new TodoItemRequest()
+            {
+                Id = createTodoItemResponse.Id,
+                Description = "Bar",
+                IsActive = false,
+                DueDate = DateTime.Today.AddDays(1),
+                RollsOver = false
+            };
+            updateTodoItemRequest.TagIds.Add(createFooTagResponse.Id);
+            updateTodoItemRequest.TagIds.Add(createBarTagResponse.Id);
+
+            await _mediator.Send(new UpdateTodoItemCommand(updateTodoItemRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var todoItems = await _mediator.Send(new GetAllTodoItemsQuery());
+            var todoItem = todoItems.Where(x => x.Id == updateTodoItemRequest.Id).First();
+
+            todoItem.Id.Should().Be(updateTodoItemRequest.Id);
+            todoItem.Description.Should().Be(updateTodoItemRequest.Description);
+            todoItem.IsActive.Should().BeFalse();
+            todoItem.RollsOver.Should().BeFalse();
+            todoItem.DueDate.Should().Be(updateTodoItemRequest.DueDate);
+            todoItem.Tags.Count.Should().Be(2);
         }
     }
 }
