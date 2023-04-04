89d3922cb72f6f003e045a62f500bc1444ec8164
Jason L. Walker <jasonlanewalker@gmail.com>
2023-04-03 19:32:26 -0500

    Implemented feature to pass test

--- CleanTodo.Core/Application/Commands/TodoTags/UpdateTodoTagCommand.cs
+++ CleanTodo.Core/Application/Commands/TodoTags/UpdateTodoTagCommand.cs
@@ -3,7 +3,9 @@ using CleanTodo.Core.Application.Interfaces.Persitence;
 using CleanTodo.Core.Application.Queries.TodoItems;
 using CleanTodo.Core.Application.Queries.TodoTags;
 using CleanTodo.Core.Entities;
+using CleanTodo.Core.Exceptions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +29,23 @@ namespace CleanTodo.Core.Application.Commands.TodoTags
 
         public async Task<TodoTagResponse> Handle(UpdateTodoTagCommand request, CancellationToken cancellationToken)
         {
+            // Verify the tag exists
             var tag = await _context.FirstOrNotFound<TodoTag>(request.Data.Id);
+
+            // See if this tag already exists, ignoring case-sensitivity
+            var duplicateCheck = await _context.TodoTags
+                .Where(tag => request.Data.Name.ToLower().Trim() == tag.Name.ToLower().Trim())
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (duplicateCheck != null && duplicateCheck.Id != request.Data.Id)
+            {
+                throw new DuplicateTagException(string.Format(
+                    "Unabled to edit tag with Id: {0} to name: {1}. A tag with this name already exists.",
+                    request.Data.Id,
+                    request.Data.Name
+                ));
+            }
+
             tag.Name = request.Data.Name;
             _context.TodoTags.Update(tag);
             await _context.SaveChangesAsync();
