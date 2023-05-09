0a4361592f6c160f377c7c8762f139f85ef4a44c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-05-08 21:53:30 -0500

    Added test to confirm completed item has today's date as a completion date

--- CleanTodo.IntegrationTests/Application/Commands/TodoItems/SetTodoItemCompletionStatusShould.cs
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/SetTodoItemCompletionStatusShould.cs
@@ -42,5 +42,23 @@ namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
 
             updateResponse.IsComplete.Should().BeFalse();
         }
+
+
+
+        [Fact]
+        public async Task GivenTodoItemMarkedComplete_WhenHandled_WillBeCompleteWithTodaysDate()
+        {
+            var createRequest = new TodoItemRequest()
+            {
+                Description = "This is a todo item."
+            };
+
+            var createResponse = await _mediator.Send(new CreateTodoItemCommand(createRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var updateResponse = await _mediator.Send(new SetTodoItemCompletionStatusCommand(createResponse.Id, true));
+
+            updateResponse.CompletionDate.Should().Be(DateTime.Today);
+        }
     }
 }
