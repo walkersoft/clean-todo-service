fcbf111f2d18891ad2ee1b8805d5646221ef683b
Jason L. Walker <jasonlanewalker@gmail.com>
2023-05-08 19:37:10 -0500

    Created failing unit test to check roll over quantity

--- CleanTodo.IntegrationTests/Application/Queries/TodoItems/GetTodoItemsShould.cs
+++ CleanTodo.IntegrationTests/Application/Queries/TodoItems/GetTodoItemsShould.cs
@@ -29,5 +29,23 @@ namespace CleanTodo.IntegrationTests.Application.Queries.TodoItems
 
             response.Any().Should().BeTrue();
         }
+
+        [Fact]
+        public async Task FetchTodoItems_WillItemsThatRollOver_WillCalculateRollOverQuantity()
+        {
+            var createRequest = new TodoItemRequest()
+            {
+                Description = "This is a todo item.",
+                DueDate = DateTime.Now.AddDays(-3),
+                RollsOver = true
+            };
+
+            await _mediator.Send(new CreateTodoItemCommand(createRequest));
+
+            _dbContext.ChangeTracker.Clear();
+            var response = await _mediator.Send(new GetAllTodoItemsQuery());
+
+            response.First().RollOverCount.Should().Be(3);
+        }
     }
 }
