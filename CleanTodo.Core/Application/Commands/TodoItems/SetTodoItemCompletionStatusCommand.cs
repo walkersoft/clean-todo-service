f93bdf10fcb851a1fc007a57ce9dab844413cc95
Jason L. Walker <jasonlanewalker@gmail.com>
2023-05-08 21:37:11 -0500

    Created failing unit test to update todo item complete status

---
+++ CleanTodo.Core/Application/Commands/TodoItems/SetTodoItemCompletionStatusCommand.cs
@@ -0,0 +1,20 @@
+﻿using CleanTodo.Core.Application.Queries.TodoItems;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Commands.TodoItems
+{
+    public record SetTodoItemCompletionStatusCommand(Guid Id, bool IsComplete) : IRequest<TodoItemResponse>;
+
+    public class SetTodoItemCompletionStatusCommandHandler : IRequestHandler<SetTodoItemCompletionStatusCommand, TodoItemResponse>
+    {
+        public Task<TodoItemResponse> Handle(SetTodoItemCompletionStatusCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/SetTodoItemCompletionStatusShould.cs
@@ -0,0 +1,31 @@
+﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
+{
+    public class SetTodoItemCompletionStatusShould : TestingBase
+    {
+        [Fact]
+        public async Task GivenTodoItemMarkedComplete_WhenHandled_WillBeCompleted()
+        {
+            var createRequest = new TodoItemRequest()
+            {
+                Description = "This is a todo item."
+            };
+
+            var createResponse = await _mediator.Send(new CreateTodoItemCommand(createRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var updateResponse = await _mediator.Send(new SetTodoItemCompletionStatusCommand(createResponse.Id, true));
+
+            updateResponse.IsComplete.Should().BeTrue();
+            updateResponse.CompletionDate.Should().Be(DateTime.Today);
+        }
+    }
+}
