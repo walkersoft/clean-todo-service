4fa88f41c7c115da07be74aef78f236b8004324c
Jason L. Walker <jasonlanewalker@gmail.com>
2023-06-06 22:04:49 -0500

    Added controller endpoint to post a new list

---
+++ CleanTodo.Api/Controllers/TodoListsController.cs
@@ -0,0 +1,27 @@
+﻿using CleanTodo.Core.Application.Commands.TodoLists;
+using CleanTodo.Core.Application.Queries.TodoLists;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTodo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodoListsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [ProducesResponseType(typeof(TodoListResponse), StatusCodes.Status200OK)]
+        [HttpPost]
+        public async Task<IActionResult> Create(TodoListRequest request)
+        {
+            return Ok(await _mediator.Send(new CreateTodoListCommand(request)));
+        }
+    }
+}
