0b7078b87c862c029694e6d60530cdc427b11954
Jason L. Walker <jasonlanewalker@gmail.com>
2023-06-13 20:05:47 -0500

    Add migration to update DB with latest schema changes

--- CleanTodo.Infrastructure/Migrations/20230614010506_Add-Title-To-TodoList.Designer.cs
+++ CleanTodo.Infrastructure/Migrations/20230614010506_Add-Title-To-TodoList.Designer.cs
@@ -4,6 +4,7 @@ using CleanTodo.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanTodo.Infrastructure.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230614010506_Add-Title-To-TodoList")]
+    partial class AddTitleToTodoList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
