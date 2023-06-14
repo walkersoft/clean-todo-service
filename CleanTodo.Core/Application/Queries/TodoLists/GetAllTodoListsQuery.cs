fb57159b15bdb428fc9d577a539cb00f00e03eef
Jason L. Walker <jasonlanewalker@gmail.com>
2023-06-13 21:39:15 -0500

    Created failing unit test to fetch todo lists

---
+++ CleanTodo.Core/Application/Queries/TodoLists/GetAllTodoListsQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Queries.TodoLists
+{
+    public record GetAllTodoListsQuery : IRequest<IEnumerable<TodoListResponse>>;
+
+    public class GetAllTodoListsQueryHandler : IRequestHandler<GetAllTodoListsQuery, IEnumerable<TodoListResponse>>
+    {
+        public Task<IEnumerable<TodoListResponse>> Handle(GetAllTodoListsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
