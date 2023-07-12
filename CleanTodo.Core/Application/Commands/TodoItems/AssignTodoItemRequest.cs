78f81d9ea197447aa2453c85bb53f44d7c9be451
Jason L. Walker <jasonlanewalker@gmail.com>
2023-07-12 00:12:11 -0500

    Added new test and command to assign todo item to a list

---
+++ CleanTodo.Core/Application/Commands/TodoItems/AssignTodoItemRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTodo.Core.Application.Commands.TodoItems
+{
+    public sealed class AssignTodoItemRequest
+    {
+        public Guid TodoItemId  { get; set; }
+        public Guid TodoListId { get; set; }
+    }
+}
---
+++ CleanTodo.IntegrationTests/Application/Commands/TodoItems/AssignTodoItemToListShould.cs
@@ -0,0 +1,48 @@
+﻿using CleanTodo.Core.Application.Commands.TodoItems;
+using CleanTodo.Core.Application.Commands.TodoLists;
+using CleanTodo.Core.Application.Queries.TodoItems;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanTodo.IntegrationTests.Application.Commands.TodoItems
+{
+    public class AssignTodoItemToListShould : TestingBase
+    {
+        [Fact]
+        public async Task GivenTodoItemAssignedToExistingList_WhenHandled_WillSucceed()
+        {
+            var createTodoItemRequest = new TodoItemRequest()
+            {
+                Description = "Todo Item"
+            };
+
+            var createTodoListRequest = new TodoListRequest()
+            {
+                Title = "Todo List"
+            };
+
+            var createTodoItemResponse = await _mediator.Send(new CreateTodoItemCommand(createTodoItemRequest));
+            var createListResponse = await _mediator.Send(new CreateTodoListCommand(createTodoListRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var assignTodoItemRequest = new AssignTodoItemRequest()
+            {
+                TodoItemId = createTodoItemResponse.Id,
+                TodoListId = createListResponse.Id
+            };
+
+            await _mediator.Send(new AssignTodoItemCommand(assignTodoItemRequest));
+            _dbContext.ChangeTracker.Clear();
+
+            var getListResponse = await _mediator.Send(new GetAllTodoItemsQuery());
+
+            getListResponse.Any(todoItem => todoItem.Id == createTodoItemResponse.Id)
+                .Should().BeTrue();
+        }
+    }
+}
